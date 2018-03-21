cba1426aa008ebc66e01cbe93e4a25b7e5bbb16a
Marissa L <marissal.io@gmail.com>
2018-03-21 11:47:01 -0400

    delete excess files

--- client/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import ToDoList from './ToDoList.jsx';
-
-render((
-    <div>
-        <ToDoList />
-    </div>
-), document.getElementById('contents'));
