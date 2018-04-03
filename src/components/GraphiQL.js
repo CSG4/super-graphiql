ed968594d2dfd3b5e5dd110540a0255ee55d651c
angelascerbo <amscerbo@gmail.com>
2018-04-03 15:32:37 -0400

    correct result style + trash can fix

--- src/components/GraphiQL.js
+++ src/components/GraphiQL.js
@@ -676,16 +676,15 @@ export class GraphiQL extends React.Component {
           variables,
           // operationName
           result => {
-            const cleanResults = result.map((resultObj, index) => {
-              resultObj["dataSet" + index] = resultObj.data;
-              delete resultObj["data"];
-              return resultObj;
-            });
+            const transformResults = result.reduce((acc, resultObj, index) => {
+              acc["dataSet" + index] = resultObj.data;
+              return acc;
+            }, {});
 
             if (runID === this._runCounter) {
               this.setState({
                 isWaitingForResponse: false,
-                response: JSON.stringify(cleanResults, null, 2)
+                response: JSON.stringify(transformResults, null, 2)
               });
             }
           }
